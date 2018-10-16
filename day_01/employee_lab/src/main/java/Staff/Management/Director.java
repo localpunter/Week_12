e783e78c8f6051c545181adebf9635be9526fec1
Alan Hunter <localpunter@sky.com>
2018-10-16 09:45:26 +0100

    all tests now passing

--- day_01/employee_lab/src/main/java/Staff/Management/Director.java
+++ day_01/employee_lab/src/main/java/Staff/Management/Director.java
@@ -2,16 +2,12 @@ package Staff.Management;
 
 public class Director extends Manager {
 
-    private String deptName;
+//    private String deptName;
     private double budget;
 
     public Director(String name, String ni, int salary, String deptName, double budget) {
         super(name, ni, salary, deptName);
         this.budget = budget;
-    }
-
-    public String getDeptName() {
-        return deptName;
     }
 
     public void raiseSalary(double percentage) {
